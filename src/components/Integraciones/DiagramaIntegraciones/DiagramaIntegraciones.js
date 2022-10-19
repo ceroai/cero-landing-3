678c5dc1c0ab8a4b8eeba7d44c484344ab06ba27
Alejandro Lavado <alejandro.lvd@gmail.com>
2022-10-19 12:55:40 -0300

    engranaje

--- src/components/Integraciones/DiagramaIntegraciones/DiagramaIntegraciones.js
+++ src/components/Integraciones/DiagramaIntegraciones/DiagramaIntegraciones.js
@@ -1,9 +1,71 @@
+import { Icon } from '@iconify/react'
 import './DiagramaIntegraciones.css'
 
+const integraciones = [
+  {
+    nombre: 'Dentalink'
+  },
+  {
+    nombre: 'Medilink'
+  },
+  {
+    nombre: 'Dentalink'
+  },
+  {
+    nombre: 'Dentalink'
+  },
+  {
+    nombre: 'Dentalink'
+  },
+  {
+    nombre: 'Dentalink'
+  },
+  {
+    nombre: 'Dentalink'
+  },
+  {
+    nombre: 'Dentalink'
+  },
+  {
+    nombre: 'Dentalink'
+  },
+  {
+    nombre: 'Dentalink'
+  },
+  {
+    nombre: 'Dentalink'
+  },
+  {
+    nombre: 'Dentalink'
+  },
+]
+
 const DiagramaIntegraciones = () => {
+
   return (
     <div className="DiagramaIntegraciones">
-      
+      <div className="DiagramaIntegraciones__contenedor_integraciones">
+        {integraciones.map(integracion => (
+          <div
+            key={integracion.nombre}
+            className="DiagramaIntegraciones__integracion"
+          >
+
+          </div>
+        ))}
+      </div>
+      <div className="DiagramaIntegraciones__division">
+        <div className="DiagramaIntegraciones__division_linea DiagramaIntegraciones__division_linea--horizontal" />
+        <div className="DiagramaIntegraciones__division_linea DiagramaIntegraciones__division_linea--vertical" />
+        <div className="DiagramaIntegraciones__engranaje">
+          <Icon icon="mdi:cog" />
+        </div>
+      </div>
+      <div className="DiagramaIntegraciones__cero">
+        <div className="DiagramaIntegraciones__logo_cero">
+          CERO
+        </div>
+      </div>
     </div>
   )
 }

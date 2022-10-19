efe7fbe804ba9a55d27494ce9cb55db2fd2caaed
Alejandro Lavado <alejandro.lvd@gmail.com>
2022-10-19 18:20:52 -0300

    capturas

--- src/components/Tecnologia/DiagramaTecnologia/DiagramaTecnologia.js
+++ src/components/Tecnologia/DiagramaTecnologia/DiagramaTecnologia.js
@@ -1,9 +1,12 @@
+import captura1 from '../../../assets/images/feedback-chat.jpg'
+import captura2 from '../../../assets/images/feedback-respuestas.jpg'
 import './DiagramaTecnologia.css'
 
 const DiagramaTecnologia = () => {
   return (
     <div className="DiagramaTecnologia">
-      
+      <img className="DiagramaTecnologia__captura" src={captura1} alt="Feedback" />
+      <img className="DiagramaTecnologia__captura DiagramaTecnologia__captura--back" src={captura2} alt="Feedback" />
     </div>
   )
 }

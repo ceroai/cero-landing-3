16d011fd6452b7b7a30e3d8142e87d20b9bd881a
Alejandro Lavado <alejandro.lvd@gmail.com>
2022-10-25 08:45:32 -0300

    link de iframe

--- src/components/Capacidad/CasosDeUso/CasosDeUso.js
+++ src/components/Capacidad/CasosDeUso/CasosDeUso.js
@@ -15,7 +15,7 @@ const CasosDeUso = () => {
         className="CasosDeUso"
         onClick={e => e.stopPropagation()}
       >
-        <iframe src="" style={{ width: '100%', height: '100%' }} title="Expo" />
+        <iframe src="https://cero-landing-2.netlify.app/expo" style={{ width: '100%', height: '100%' }} title="Expo" />
       </div>
     </div>
     
